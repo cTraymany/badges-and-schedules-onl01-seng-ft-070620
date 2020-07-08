582f043b5fa8f9731e479b9b1f41f3652da68273
Chindalath Traymany <chindalatht@gmail.com>
2020-07-08 04:03:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
+def assign_rooms(speakers)
+  welcome = []
+  speakers.each_with_index do |name, index|
+    welcome << "Hello, #{name}! You'll be assigned to room #{index += 1}!"
+  end
+  welcome
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |introduction|
+    puts introduction
+  end
+  assign_rooms.each do 
+end
+
+
+assign_rooms(["Dan", "Sam", "Bree"])
+
+
